45af763f5e63cd113ce629795b24deadd7f2b4e5
BHISM <61289719+bhism@users.noreply.github.com>
2021-10-12 13:39:12 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:imageapp/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "image pick app",
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: Home(),
+    );
+  }
+}
